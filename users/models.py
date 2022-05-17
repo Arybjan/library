c016ad36facc389d65bfba70b4840f007c3bbc47
arybjan <arybjen92@gmail.com>
2022-05-17 17:31:29 +0600

    add table on users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(_('ФИО'), max_length=250)
+    username = models.CharField(max_length=150)
+    email = models.EmailField(_("Почта"), unique=True)
+    birthday_date = models.DateField()
+    gender_id = models.ForeignKey("users.Genders", on_delete=models.SET_NULL, null=True)
+    occupation_id = models.ForeignKey("", on_delete=models.CASCADE)
+    city_id = models.ForeignKey("", on_delete=models.CASCADE)
+    role_id = models.ForeignKey("", on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+
+    USERNAME_FIELD = "username"
+
+    class Meta:
+        verbose_name = _("Пользователь")
+        verbose_name_plural = _("Пользователи")
+
+    def __str__(self):
+        return self.full_name
+
+
+class Genders(models.Model):
+    name = models.CharField(_("Пол"), max_length=100)
+
+
+class Roles(models.Model):
+    name = models.CharField(_("Роль"), max_length=150)
+
+
+class Cities(models.Model):
+    name = models.CharField(_("Город"), max_length=100)
+    country_id = models.ForeignKey("users.Counrty", on_delete=models.CASCADE)
+
+
+class Country(models.Model):
+    name = models.CharField(_("Страна"), max_length=80)
+
+
+class Occupations(models.Model):
+    name = models.CharField(_("Занятость"), max_length=150)
