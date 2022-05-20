b9208c2cdc7349bf2e181832c21289682c95ed13
arybjan <arybjen92@gmail.com>
2022-05-20 12:17:50 +0600

    added views for authors fields

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from authors import models
+from authors.serializer import (
+    AuthorSerializer,
+    AuthorBookSerializer,
+    FavoriteAuthorsSerializer
+)
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = models.Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorBookViewSet(viewsets.ModelViewSet):
+    queryset = models.AuthorBook.objects.all()
+    serializer_class = AuthorBookSerializer
+
+
+class FavoriteAuthors(viewsets.ModelViewSet):
+    queryset = models.FavoriteAuthors.objects.all()
+    serializer_class = FavoriteAuthorsSerializer
