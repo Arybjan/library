dce0a2f3784c3a0ce324121e37b74eecfddb3527
arybjan <arybjen92@gmail.com>
2022-05-23 02:36:18 +0600

    fixet bug

--- library/settings.py
+++ library/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static'
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static'
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
